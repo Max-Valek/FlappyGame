629902c38ca164a4a0bc48fab2e76c89734b1194
Max Valek <valek.maxwell@gmail.com>
2023-04-24 14:40:17 -0500

    Initial Commit

---
+++ FlappyGame/FlappyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyGameApp.swift
+//  FlappyGame
+//
+//  Created by Max Valek on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FlappyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
